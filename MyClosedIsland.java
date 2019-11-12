e7b546250d1588511dfa2c6f8e61f6e6beb85d63
Caleb Chang <changc42@gmail.com>
2019-11-11 23:56:02 -0500

    Update MyClosedIsland.java

--- MyClosedIsland.java
+++ MyClosedIsland.java
@@ -4,7 +4,7 @@ class MyClosedIsland{
 	int[][] map;
 	int rows;
 	int cols;
-	ArrayList<ArrayList<Object>> rOS;//Object can only be int[] or Integer. rOS=rootOrSize
+	Object[][] rOSMap;//Object can only be int[] or Integer. rOSMap=rootOrSize Map
 	
 	MyClosedIsland(int[][] input){
 		map=input;
@@ -12,45 +12,74 @@ class MyClosedIsland{
 		cols=input[0].length;
 		
 		for(int i=0; i<rows; i++){
-			rOS.add(new ArrayList<Object>());
 			for(int j=0; j<cols; j++){
-				rOS.get(i).add(new Integer(1));
+				if(input[i][j]==1)
+					rOSMap[i][j]=new Integer(0);
+				if(input[i][j]==0)
+					rOSMap[i][j]=new Integer(1);
 			}
 		}
 	}
 	
-	class ROS{
-		
-	}
-	
 	static Object numIslands(){
 		int ans=0;
-		
+		for(int i=0; i<rows; i++){
+			for(int j=0; j<cols; j++){
+				if(map[i][j]==0 && map[i][j+1]==0 && j+1<cols) unify(i,j,i,j+1);
+				if(map[i][j]==0 && map[i+1][j]==0 && i+1<rows) unify(i,j,i+1,j);
+			}
+		}
+		for(int i=1; i<rows-1; i++){
+			for(int j=1; j<cols-1; j++){
+				if(rOSMap[i][j] instanceof Integer){
+					if(rOSMap[i][j]>=1)
+						ans++;
+				}
+			}
+		}
 		return ans;
 	}
 	
-	Integer getSize(int r, int c){
-		if(rOS.get(r).get(c) instanceof Integer) return rOS.get(r).get(c);
-		else return getSize(rOS.get(r).get(c)[0], rOS.get(r).get(c)[1]);
+	
+	boolean isEdge(int r, int c){
+		return (r==0 || r== rows-1 || c==0 || c==cols-1)
 	}
 	
-	
 	void unify(int r1, int c1, int r2, int c2){
-		//if second position is on edge, swap values so edge position becomes root
-		if(r2==0 || r2==rows-1 || c2==0 || c2==cols-1){
+		//case1: (r1,c1) is not edge and (r2,c2) is edge
+		if(!isEdge(r1,c1) && isEdge(r2,c2)){
 			int temp1;
 			temp1=r1;
 			r1=r2;
 			r2=temp1;
 			int temp2;
-			temp2=c1;
-			c1=c2;
+			temp2=r2;
+			r2=c2;
 			c2=temp2;
 		}
-		if(rOS.get(r1).get(c1) instanceof int[]){
-			rOS.get(rOS.get(r1).get(c1)[0]).get(rOS.get(r1).get(c1)[1])
-			rOS.get(r2).set(c2,new int[]{r1,c1});
-		}			
+		
+		Integer y = rOSMap[rOSMap[r2][c2][0]][rOSMap[r2][c2][1]];
+		//case2: x is root and y is root
+		if(rOSMap[r1][c1] instanceof Integer && rOSMap[r2][c2] instanceof Integer){
+			rOSMap[r1][c1] += rOSMap[r2][c2];
+			rOSMap[r2][c2] = new int[][]{r1,c1};
+		}
+		//case3: x is descendant and y is root
+		if(rOSMap[r1][c1] instanceof int[][] && rOSMap[r2][c2] instanceof Integer){
+			rOSMap[rOSMap[r1][c1][0]][rOSMap[r1][c1][1]] += rOSMap[r2][c2];
+			rOSMap[r2][c2] = new int[][]{rOSMap[r1][c1][0], rOSMap[r1][c1][1]};
+		}
+		//case4:x is descendant and y is descendant
+		if(rOSMap[r1][c1] instanceof int[][] && rOSMap[r2][c2] instanceof int[][]){
+			rOSMap[rOSMap[r1][c1][0]][rOSMap[r1][c1][1]] += rOSMap[rOSMap[r2][c2][0]][rOSMap[r2][c2][1]]
+			rOSMap[rOSMap[r2][c2][0]][rOSMap[r2][c2][1]] = new int[][]{rOSMap[r1][c1][0], rOSMap[r1][c1][1]};
+		}
+		//case 5: x is root and y is descendant
+		if(rOSMap[r1][c1] instanceof Integer && rOSMap[r2][c2] instanceof int[][]){
+			rOSMap[r1][c1] += rOSMap[rOSMap[r2][c2][0]][rOSMap[r2][c2][1]];
+			rOSMap[rOSMap[r2][c2][0]][rOSMap[r2][c2][1]] = new int[][]{r1,c1};
+		}
+				
 	}
 	
 	
