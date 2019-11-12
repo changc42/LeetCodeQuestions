fbae592fe0a33c7ed576743db754c5e1853f6de4
Caleb Chang <changc42@gmail.com>
2019-11-12 15:27:51 -0500

    Create MaxScoreFromLetters.java

---
+++ MaxScoreFromLetters.java
@@ -0,0 +1,30 @@
+/*
+Given a list of words, list of  single letters (might be repeating) and score of every character.
+
+Return the maximum score of any valid set of words formed by using the given 
+letters (words[i] cannot be used two or more times).
+
+It is not necessary to use all characters in letters and each letter can only be used once.
+Score of letters 'a', 'b', 'c', ... ,'z' is given by score[0], score[1], ... , score[25] respectively.
+*/
+
+class MaxScoreFromLetters{
+	String[] words;
+	LinkedList<char> letters;
+	int[] score;
+	
+	MaxScoreFromLetters(String[] w, LinkedList<char> l, int[] s){
+		words=w;
+		letters=l;
+		score=s;
+	}
+	
+	class Word{
+		String str;
+		int score;
+		
+		Word(String s){
+			
+		}
+	}
+}
