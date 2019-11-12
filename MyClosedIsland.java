c175ee7e40255f7adc0f80202f5b06e7d8a0cc1e
Caleb Chang <changc42@gmail.com>
2019-11-11 22:47:24 -0500

    Update MyClosedIsland.java

--- MyClosedIsland.java
+++ MyClosedIsland.java
@@ -4,7 +4,7 @@ class MyClosedIsland{
 	int[][] map;
 	int rows;
 	int cols;
-	ArrayList<ArrayList<Object>> rootOrSize;//Object can only be int[] or Integer
+	ArrayList<ArrayList<Object>> rOS;//Object can only be int[] or Integer. rOS=rootOrSize
 	
 	MyClosedIsland(int[][] input){
 		map=input;
@@ -12,24 +12,47 @@ class MyClosedIsland{
 		cols=input[0].length;
 		
 		for(int i=0; i<rows; i++){
-			rootOrSize.add(new ArrayList<Object>());
+			rOS.add(new ArrayList<Object>());
 			for(int j=0; j<cols; j++){
-				rootOrSize.get(i).add(new Integer(1));
+				rOS.get(i).add(new Integer(1));
 			}
 		}
 	}
 	
+	class ROS{
+		
+	}
+	
 	static Object numIslands(){
-		Integer ans=new Integer(0);
+		int ans=0;
 		
 		return ans;
 	}
-	/*
-	Object getRootOrSize(int r, int c){
-		if(rootOrSize.get(r).get(c) instanceof Integer) return rootOrSize.get(r).get(c);
-		else return 
+	
+	Integer getSize(int r, int c){
+		if(rOS.get(r).get(c) instanceof Integer) return rOS.get(r).get(c);
+		else return getSize(rOS.get(r).get(c)[0], rOS.get(r).get(c)[1]);
 	}
-	*/
+	
+	
+	void unify(int r1, int c1, int r2, int c2){
+		//if second position is on edge, swap values so edge position becomes root
+		if(r2==0 || r2==rows-1 || c2==0 || c2==cols-1){
+			int temp1;
+			temp1=r1;
+			r1=r2;
+			r2=temp1;
+			int temp2;
+			temp2=c1;
+			c1=c2;
+			c2=temp2;
+		}
+		if(rOS.get(r1).get(c1) instanceof int[]){
+			rOS.get(rOS.get(r1).get(c1)[0]).get(rOS.get(r1).get(c1)[1])
+			rOS.get(r2).set(c2,new int[]{r1,c1});
+		}			
+	}
+	
 	
 	
 	
