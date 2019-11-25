e1adaed6a253bf8746782c544e5ee8ef11644654
Caleb Chang <changc42@gmail.com>
2019-11-25 17:42:27 -0500

    s

--- test.java
+++ test.java
@@ -1,8 +1,20 @@
+import java.util.*;
+
 class test{
 	public static void main(String[] args){
-		String s="hello";
-		String sub = s.substring(0,1);
-		System.out.println(sub);
+		String i = "huupp";
+		String j=i;
+		i += "opoway";
+		System.out.println("i="+i+" j=" +j);
+		
+		ArrayList<Integer> a = new ArrayList<>();
+		a.add(100);
+		ArrayList<Integer> b = a;
+		a.add(4);
+		System.out.println(a + "\n" + b);
+		
+		String s= "hello";
+		System.out.println(s);
 	}
 
 }
