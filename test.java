bcf5c831f2c1c326d3c425bc1a0d36fbce48cae8
Caleb Chang <changc42@gmail.com>
2019-11-29 10:25:49 -0500

    gv

--- test.java
+++ test.java
@@ -1,17 +1,35 @@
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.*;
 
 class test{
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for(int i=0; i<nums.length-3; i++){
+            for(int j=i+1; j<nums.length-2;j++){
+                int SI = j+1;
+                int EI = nums.length-1;
+                while(SI<EI){
+					System.out.println("SI="+SI+ " EI=" +EI);
+                    if(nums[i]+nums[j]+nums[SI]+nums[EI]<target) SI++;
+                    else if(nums[i]+nums[j]+nums[SI]+nums[EI]>target) EI--;
+                    else{
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[SI]);
+                        temp.add(nums[EI]);
+                        ans.add(temp);
+						SI++;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
 	public static void main(String[] args){
-		CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
-		for(int i=0; i<10; i++){
-			list.add(String.valueOf(i));
-		}
-		
-		for(String s: list){
-			list.add(s+"!!");
-			list.remove("3");
-		}
-		
-		System.out.println(list);
+		int[] a=new int[]{-2,-1,1,2,-3,3,10};
+		test b = new test();
+		System.out.println(b.fourSum(a,0));
 	}
 }
