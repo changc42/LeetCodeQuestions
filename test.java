43f1868b7a955baf23b35ae777c79768967e031a
Caleb Chang <changc42@gmail.com>
2020-01-10 01:39:32 -0500

    tested String.split() and String.equals()

--- test.java
+++ test.java
@@ -1,11 +1,25 @@
 import java.util.*;
-
+/*
+class test{
+	static List<String> toCorrectList(String[] arr){
+        List<String> list = new LinkedList<>(Arrays.asList(arr));
+        
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i)=="+");
+        }
+        
+        return list;
+    }
+	public static void main(String[] args){
+		String[] arr = new String[]{"1","3","+","4"};
+		toCorrectList(arr);
+	}
+}
+*/
 class test{
 	public static void main(String[] args){
-		Hashtable<String, Integer> a = new Hashtable<>();
-		String s = "hello";
-		a.put(s,2);
-		a.put(s,3);
-		System.out.println(a.entrySet());
+		String s = "3+2";
+		String s2= new String("3+2");
+		System.out.println(s.equals(s2));
 	}
 }
